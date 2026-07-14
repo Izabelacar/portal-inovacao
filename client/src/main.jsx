4d49a629a4cc8972b3bda7afd465287ea4aac222
izabelacar <izabelacristinac@gmail.com>
2026-07-14 20:22:21 -0300

    ajustes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import { HashRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './styles/style.css';
-
-// HashRouter (em vez de BrowserRouter) para funcionar de forma confiável no
-// GitHub Pages, que não sabe redirecionar rotas como /mapas ou /admin para o
-// index.html. As URLs ficam com # (ex: seusite.github.io/repo/#/mapas).
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-=======
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './styles/style.css';
@@ -23,6 +9,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
->>>>>>> f62d7863e58360d65edb3f16471c986938deda61
   </React.StrictMode>
 );
